76bbed0fb226c538b9a9d7f64b70cc6735c2544b
DiegoAndresGlez <diegoglez077@gmail.com>
2023-11-18 23:10:17 -0800

    Mi Planta - Falta iconos

--- src/pages/MiPlanta/MiPlanta.jsx
+++ src/pages/MiPlanta/MiPlanta.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
-import { Panel, PanelGroup} from 'rsuite';
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from "recharts";
+import { Panel} from 'rsuite';
 import {Button, ButtonGroup} from 'rsuite';
 import { Progress, Row, Col } from 'rsuite';
 import { Stack } from "rsuite";
-import Container from 'rsuite/Container';
 
 export default function MiPlanta() {
   return (
     <>    
-      <PanelGroup>
-        <UpperPanel />
-      </PanelGroup>
+      <UpperPanel />
       <LowerPanel />
     </>
   );
@@ -19,8 +25,8 @@ export default function MiPlanta() {
 const UpperPanel = () => {
   return (
     <>
-      <Panel>
-        <Stack spacing={6}>
+      <Panel shaded>
+        <Stack justifyContent="flex-start" spacing={6} style={{marginLeft: 75}}>
           <div>
             <label
               style={{
@@ -57,7 +63,8 @@ const UpperPanel = () => {
                     backgroundColor: "#24A148",
                   }}>
                     <p style={{ 
-                      color: "#f2f2f2"
+                      color: "#f2f2f2",
+                      fontWeight: "bold",
                     }}>Editar Planta</p>
               </Button>
             </div>
@@ -71,30 +78,43 @@ const UpperPanel = () => {
                     backgroundColor: "#796236",
                   }}>
                     <p style={{ 
-                      color: "#f2f2f2"
+                      color: "#f2f2f2",
+                      fontWeight: "bold",
                     }}>Eliminar Planta</p>
               </Button>
             </div>
         </Stack>
       </Panel>
-      <hr />
     </>
   );
 }
 
 const LowerPanel = () => {
   return (
-    <Stack spacing={6} style={{marginLeft: 150}}>
-      <HumedadMaxima/>
-      <HumedadMinima/>
-      <TemperaturaMaxima/>
-      <TemperaturaMinima/>
-    </Stack>
+    <>
+      <Stack justifyContent="center" style={{marginTop: 25}}>
+        <Stack direction="column" spacing={4}>
+          <HumedadMaxima/>
+        </Stack>
+        <Stack direction="column" spacing={4}>
+          <HumedadMinima/>
+        </Stack>
+        <Stack direction="column" spacing={4}>
+          <TemperaturaMaxima/>
+        </Stack>
+        <Stack direction="column" spacing={4}>
+          <TemperaturaMinima/>
+        </Stack>
+      </Stack>
+      <Stack justifyContent="center" style={{marginTop: 50}}>
+        <Dispositivo/>
+      </Stack>
+    </>
   )
 }
 
 const HumedadMaxima = () => {
-  const [percent, setPercent] = React.useState(10);
+  const [percent, setPercent] = React.useState(50);
 
   const decline = () => {
     const value = Math.max(percent - 10, 0);
@@ -111,19 +131,19 @@ const HumedadMaxima = () => {
 
   return (
     <>
-      <ButtonGroup style={{marginLeft: 100}}>
+      <ButtonGroup style={{}}>
         <Button onClick={decline}>-</Button>
         <Button onClick={increase}>+</Button>
       </ButtonGroup>
 
       <Row>
         <Col md={6}>
-          <div style={{ width: 180, marginTop: 10, marginLeft: 100 }}>
+          <div style={{ width: 180, marginTop: 10, display: "block"}}>
             <Progress.Circle percent={percent} strokeColor={color} status={status} />
           </div>
         </Col>
       </Row>
-      <label style={{marginLeft: 90, marginTop: 24, fontSize: 24}}>
+      <label style={{marginTop: 12, fontSize: 24, display: "block"}}>
         Humedad Máxima
       </label>
     </>
@@ -131,7 +151,7 @@ const HumedadMaxima = () => {
 }
 
 const HumedadMinima = () => {
-  const [percent, setPercent] = React.useState(10);
+  const [percent, setPercent] = React.useState(15);
 
   const decline = () => {
     const value = Math.max(percent - 10, 0);
@@ -155,12 +175,12 @@ const HumedadMinima = () => {
 
       <Row>
         <Col md={6}>
-          <div style={{ width: 180, marginTop: 10, marginLeft: 100 }}>
+          <div style={{ width: 180, marginTop: 10, marginLeft: 100, display: "block" }}>
             <Progress.Circle percent={percent} strokeColor={color} status={status} />
           </div>
         </Col>
       </Row>
-      <label style={{marginLeft: 95, marginTop: 24, fontSize: 24}}>
+      <label style={{marginLeft: 95, marginTop: 12, fontSize: 24, display: "block"}}>
         Humedad Mínima
       </label>
     </>
@@ -170,10 +190,10 @@ const HumedadMinima = () => {
 const TemperaturaMaxima = () => {
   return(
     <>
-      <label style={{fontSize: 48, marginLeft: 150, display: "block"}}>
+      <label style={{fontSize: 48, marginLeft: 80, marginTop: 128, paddingBottom: 40, display: "block"}}>
         35°C
       </label>
-      <label style={{fontSize: 24, marginLeft: 95}}>
+      <label style={{fontSize: 24, marginLeft: 95, display: "block"}}>
         Temperatura Máxima
       </label>
     </>
@@ -183,12 +203,76 @@ const TemperaturaMaxima = () => {
 const TemperaturaMinima = () => {
   return(
     <>
-      <label style={{fontSize: 48, marginLeft: 125, display: "block"}}>
+      <label style={{fontSize: 48, marginLeft: 80, marginTop: 128, paddingBottom: 40, display: "block"}}>
         18°C
       </label>
-      <label style={{fontSize: 24, marginLeft: 75}}>
+      <label style={{fontSize: 24, marginLeft: 75, display: "block"}}>
         Temperatura Mínima
       </label>
     </>
   )
+}
+
+const data = [
+  {
+    name: "11/11/2023",
+    temperatura: 28,
+    humedad: 56,
+    amt: 2400,
+  },
+  {
+    name: "12/11/2023",
+    temperatura: 29,
+    humedad: 64,
+    amt: 2210,
+  },
+  {
+    name: "13/11/2023",
+    temperatura: 27,
+    humedad: 60,
+    amt: 2290,
+  },
+  {
+    name: "14/11/2023",
+    temperatura: 30,
+    humedad: 58,
+    amt: 2000,
+  },
+  {
+    name: "15/11/2023",
+    temperatura: 27,
+    humedad: 67,
+    amt: 2181,
+  },
+  {
+    name: "16/11/2023",
+    temperatura: 28,
+    humedad: 67,
+    amt: 2500,
+  },
+  {
+    name: "17/11/2023",
+    temperatura: 29,
+    humedad: 58,
+    amt: 2100,
+  },
+];
+
+const Dispositivo = () => {
+  return (
+    <LineChart
+      width={730}
+      height={250}
+      data={data}
+      margin={{ top: 5, right: 30, left: 20, bottom: 5 }}
+    >
+      <CartesianGrid strokeDasharray="3 3" />
+      <XAxis dataKey="name" />
+      <YAxis />
+      <Tooltip />
+      <Legend />
+      <Line type="monotone" dataKey="temperatura" stroke="#ff3b30" />
+      <Line type="monotone" dataKey="humedad" stroke="#4285f4" />
+    </LineChart>
+  );
 }
