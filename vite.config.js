0cc782be160454ec1740a64264bc282a060678dd
egrojtorrez <egrojtorrez@gmail.com>
2023-11-19 18:36:05 -0800

    ✔ eliminando workflow

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'node:path'
 
 export default defineConfig({
   plugins: [react()],
-  // base: '/growSmartFrontend/',
+  base: '/growSmartFrontend/',
   resolve: {
     alias: {
       '@src': path.resolve(__dirname, 'src'),
